79c4f8ad518894e6fc38ff115ea304cdaadabfc8
hengmuling <anhengbin@icloud.com>
2018-08-12 21:00:05 +0800

    add some optional function

---
+++ test/Mnist_conv.py
@@ -0,0 +1,32 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+sess = tf.InteractiveSession()
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return initial
+
+
+def conv2d(x, w):  # define conv function so that we can use it many time
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# define input data
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+x_image=tf.reshape(x,[-1,28,28,1])#chane 1D data into 2D ,[N,H,W,C]
+
+
+#define
