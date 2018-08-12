a8e2d3c70e26933e9c0d3bdd7297c2d0342f4398
hengmuling <anhengbin@icloud.com>
2018-08-13 01:29:20 +0800

    add some optional function

--- test/Mnist_conv.py
+++ test/Mnist_conv.py
@@ -16,7 +16,7 @@ def bias_variable(shape):
 
 
 def conv2d(x, w):  # define conv function so that we can use it many time
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
@@ -26,7 +26,64 @@ def max_pool_2x2(x):
 # define input data
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32, [None, 10])
-x_image=tf.reshape(x,[-1,28,28,1])#chane 1D data into 2D ,[N,H,W,C]
+x_image = tf.reshape(x, [-1, 28, 28, 1])  # chane 1D data into 2D ,[N,H,W,C]
 
+# define first layer conv
+w_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-#define
+# define second layer conv
+
+w_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# define fully connected layer
+
+w_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool_flat, w_fc1) + b_fc1)
+
+# dropout method
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+w_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+
+# define loss function
+
+cross_entry = tf.reduce_mean(-tf.reduce_mean(y * tf.log(y_conv),
+                                             reduction_indices=[1]))
+
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entry)
+
+# define accuracy
+correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# kick the training
+
+tf.global_variables_initializer().run()
+
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y: batch[1],
+                                                  keep_prob: 1.0
+                                                  })
+        print("step %d,training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y: batch[1],
+                              keep_prob: 0.5
+                              })
+
+# after training is finished ,predict
+
+print("test accuracy %g" % accuracy.eval(feed_dict={x: batch[0], y: batch[1],
+                                                    keep_prob: 1.0
+                                                    }))
